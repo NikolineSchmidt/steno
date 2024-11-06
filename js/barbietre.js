f017194d62f6a8a5a4541d0de2c5a832916d3494
NikolineSchmidt <eaa23nsch@students.eaaa.dk>
2024-11-06 14:02:59 +0100

    tilføjet kommentar til barbietre
    herunder html, css og js

--- js/barbietre.js
+++ js/barbietre.js
@@ -1,5 +1,6 @@
-"use strict;"
+"use strict";  
 
+// Funktion til at gå tilbage i browserens historik
 function goBack() {
-    window.history.back();
+    window.history.back();  // Går tilbage til den forrige side i browserens historik
 }
